0316ab782586a33c4efa43bb7199f196cc036031
tc <ychernovskaya@gmail.com>
2023-10-25 13:19:48 +0700

    delete weather component

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,15 +1,13 @@
 import SearchForm from "../components/SearchForm"
 import MapComponent from "../components/MapComponent"
 import "../styles/search-page.css"
-import WeatherComponent from "../components/WeatherComponent";
+
 
 const SearchPage = () => {
     return (
         <div className='container_'>
             <div className="block1"><SearchForm/></div>
-            <div className="block2">
-                <MapComponent/><WeatherComponent/>
-            </div>
+            <div className="block2"><MapComponent/></div>
         </div>
     );
 };
